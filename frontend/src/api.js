2ff33eac887986f81811d8d50f04a1cb43638c78
M.R.MAYUNIKA <RuwandiMayunika01@gmail.com>
2025-07-05 15:44:19 +0530

    integrate frontend with backend and containarized them

---
+++ frontend/src/api.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = 'http://localhost:5000/api/todo';
+
+export default API_BASE_URL;
--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,27 +1,74 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/todolist.css';
+import no_data_image from '../images/no-data-found.png';
+import API_BASE_URL from '../api';
 
 function TodoList() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [todoList, setTodoList] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [formErrors, setFormErrors] = useState({});
 
-    const addTodo = (e) => {
-        e.preventDefault();
-        console.log(title);
-        console.log(description);
-        setTodoList([
-            { title, description, isDone: false, createdAt: Date.now() },
-            ...todoList
-        ]);
-        setTitle('');
-        setDescription('');
+    const fetchTodos = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch(`${API_BASE_URL}`);
+            if (!res.ok) throw new Error('Failed to fetch todos');
+            const data = await res.json();
+            setTodoList(data);
+        } catch (err) {
+            console.log(err.message);
+        } finally {
+            setLoading(false);
+        }
     }
 
-    const handleComplete = (index) => {
-        const updatedTodoList = [...todoList];
-        updatedTodoList[index].isDone = true;
-        setTodoList(updatedTodoList);
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const addTodo = async (e) => {
+        const newErrors = {};
+        e.preventDefault();
+        if(title == '') {
+            newErrors.titleError = true;
+        }
+        if(description == '') {
+            newErrors.descriptionError = true;
+        }
+        setFormErrors(newErrors);
+        if(title != '' && description!= '') {
+            try {
+                const res = await fetch(`${API_BASE_URL}`, {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({title, description})
+                });
+
+                if (!res.ok) throw new Error('Failed to add todo');
+
+                console.log('Todo added successfully!');
+                setTitle('');
+                setDescription('');
+                await fetchTodos();
+            } catch (err) {
+                console.log(err.message);
+            }
+        }
+    };
+
+    const markAsDone = async (index, id) => {
+        try {
+            const res = await fetch(`${API_BASE_URL}/${id}`, {
+                method: 'PUT',
+            });
+            if(!res.ok) throw new Error('Failed to update todo');
+
+            await fetchTodos();
+        } catch (err) {
+            console.log(err.message);
+        }
     }
 
     return (
@@ -36,16 +83,16 @@ function TodoList() {
                             placeholder="Title" 
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
-                            required
                         />
+                        {formErrors.titleError && <span className="error-msg">*Task title cannot be empty.</span>}
                         <textarea 
                             type="text" 
                             name="description" 
                             placeholder="Description" 
                             value={description}
                             onChange={(e) => setDescription(e.target.value)}
-                            required
                         />
+                        {formErrors.descriptionError && <span className="error-msg">*Task description cannot be empty.</span>}
                         <button type="submit" className="todo-form-button">
                             Add
                         </button>
@@ -53,11 +100,21 @@ function TodoList() {
                 </div>
                 <div className="verticle-line"></div>
                 <div className="todo-list-container">
-                    {todoList
-                        .filter(item => !item.isDone)
-                        .sort((a, b) => b.createdAt - a.createdAt)
-                        .slice(0, 5)
-                        .map((item, index) => (
+                    {loading && 
+                    <div className="loader-container">
+                        <div className="loader"></div>
+                        <p className="loader-text">Loading...</p>
+                    </div>
+                    }
+                    {!loading && todoList.length === 0 && (
+                        <div className="no-data-container">
+                            <img src={no_data_image} alt="no-data-found-img" className="not-found-image"></img>
+                            <p className="no-data-text">No uncompleted tasks found.</p>
+                        </div>
+                    )}
+                    {!loading && todoList.length > 0 &&
+                    (
+                        todoList.map((item, index) => (
                             <div className="todo-item-container" key={index}>
                                 <div className="item-details-container">
                                     <span className="todo-item-title">{item.title}</span>
@@ -65,13 +122,13 @@ function TodoList() {
                                     <span className="todo-item-description">{item.description}</span>
                                 </div>
                                 <div className="button-container">
-                                    <button className="todo-complete-button" onClick={() => handleComplete(index)}>
+                                    <button className="todo-complete-button" onClick={() => markAsDone(index, item.id)}>
                                         Done
                                     </button>
                                 </div>
                             </div>
                         ))
-                    }
+                    )}
                 </div>
             </div>
         </div>
